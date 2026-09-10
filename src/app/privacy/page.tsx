b5cf438ab306d36dfafb205aba175c3331af8f9e
ShantoSarkar34 <shanto9070sarkar@gmail.com>
2026-09-10 19:44:54 +0600

    contact, privacy and trums demo page added

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,17 @@
+import { Footer } from "@/components/shared/footer";
+import { Navbar } from "@/components/shared/navbar";
+import React from "react";
+
+function page() {
+  return (
+    <div>
+      <Navbar />
+      <div className="py-10 text-center text-4xl text-gray-500 lg:py-28">
+        Privacy page will coming soon..!
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default page;
