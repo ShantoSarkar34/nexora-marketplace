e23933457726b3d1edcf56959e445b6b90ad79d8
ShantoSarkar34 <shanto9070sarkar@gmail.com>
2026-09-21 20:35:01 +0600

    move contact, privacy and terms page on (public) folder

--- src/app/(public)/contact/page.tsx
+++ src/app/(public)/contact/page.tsx
@@ -5,11 +5,9 @@ import React from "react";
 function page() {
   return (
     <div>
-      <Navbar />
       <div className="py-10 text-center text-4xl text-gray-500 lg:py-28">
         Contact page will coming soon..!
       </div>
-      <Footer />
     </div>
   );
 }
